fcdd92d32dd8e6b745b6a7fa95ee1cce31491b17
Christopher Wilson <cwilson@cdwilson.us>
2013-07-28 14:06:48 -0700

    Add java.sh

---
+++ HOME/.profile.d/java.sh
@@ -0,0 +1,7 @@
+# ----------------------------------------------------------------------
+# Java
+# ----------------------------------------------------------------------
+
+# export JAVA_HOME="$(/usr/libexec/java_home)"
+# export JAVA_HOME=/Library/Java/Home
+# export PATH="$JAVA_HOME/bin:$PATH"
