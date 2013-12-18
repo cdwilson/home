4ee9687ed262b70a31144e5c0301e41002e7f420
Christopher Wilson <cwilson@cdwilson.us>
2013-12-18 14:18:08 -0800

    Add coreutils comments to macports.sh

--- HOME/.profile.d/macports.sh
+++ HOME/.profile.d/macports.sh
@@ -4,6 +4,12 @@
 
 PATH="/opt/local/bin:/opt/local/sbin:$PATH"
 
-# Coreutils
-# (if you want to use the GNU tools by default)
-# export PATH="/opt/local/libexec/gnubin/:$PATH"
+# ----------------------------------------------------------------------
+# coreutils
+# ----------------------------------------------------------------------
+
+# The tools provided by GNU coreutils are prefixed with the character 'g' by default to distinguish them from the BSD commands.
+# For example, cp becomes gcp and ls becomes gls.
+
+# If you want to use the GNU tools by default, add this directory to the front of your PATH environment variable:
+#     /opt/local/libexec/gnubin/
