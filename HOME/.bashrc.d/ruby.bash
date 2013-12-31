3d2633c657f55c70a576d67a677ba7451e3e4a96
Christopher Wilson <cwilson@cdwilson.us>
2013-12-31 14:39:36 -0800

    move rvm.bash to ruby.bash

--- HOME/.bashrc.d/ruby.bash
+++ HOME/.bashrc.d/ruby.bash
@@ -3,6 +3,6 @@
 # ----------------------------------------------------------------------
 
 # Load RVM into a shell session *as a function*
-if [ -r "$HOME/.rvm/scripts/rvm" ]; then
-  . "$HOME/.rvm/scripts/rvm"
-fi
+# if [ -r "$HOME/.rvm/scripts/rvm" ]; then
+#   . "$HOME/.rvm/scripts/rvm"
+# fi
