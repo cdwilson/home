23d5faa03a24fcf45c5c79460c7a324d6351f688
Christopher Wilson <cwilson@cdwilson.us>
2014-01-03 15:16:40 -0800

    Fix check for pip

--- HOME/.bashrc.d/python.bash
+++ HOME/.bashrc.d/python.bash
@@ -37,6 +37,6 @@ export PS1="${PS1}"'$(__pyenv_ps1 "'"${PS1_PYENV_FORMAT_STRING}"'")'
 # pip
 # ----------------------------------------------------------------------
 
-if ! [ command -v pip >/dev/null 2>&1 ]; then
+if ! command -v pip >/dev/null 2>&1; then
     eval "$(pip completion --bash)"
 fi
