968fbe88f3de6e67a7d2756d7e50ef36737b95fe
Chris Wilson <christopher.david.wilson@gmail.com>
2022-01-06 20:45:03 -0800

    Clean up ~/bin

--- HOME/bin/usb_find_devices.py
+++ HOME/bin/usb_find_devices.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
+import sys
 import os
 import usb.core
 
-# only needed for MacPorts
-# os.environ['DYLD_FALLBACK_LIBRARY_PATH'] = '/opt/local/lib'
+if sys.platform == "darwin":
+    os.environ["DYLD_FALLBACK_LIBRARY_PATH"] = "/opt/homebrew/lib/:/usr/local/lib:/opt/local/lib"
 
 os.environ['PYUSB_DEBUG'] = 'debug'
 print(usb.core.find())
